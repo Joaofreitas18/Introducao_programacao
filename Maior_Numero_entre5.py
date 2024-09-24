397b40053d26551b5cd015ef1379a3b5cca821f6
Joaofreitas18 <joaofreitasff4@gmail.com>
2024-09-24 15:51:49 -0300

    Introdução_Programação.py
    Primeira atualização destes códigos.

---
+++ Maior_Numero_entre5.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia 5 números e informe o maior número
+
+lista = []
+n1, n2, n3, n4, n5 = str(input("Digite cinco valores: ")).split() # split serve para receber varios itens na mesma entrada
+lista.extend([n1,n2,n3,n4,n5]) # extend serve para adicionar muitas variaveis na mesma lista
+lista.sort() # sort serve para ordenar crescentemente a lista
+print("O maior número da lista é: ", lista[4])
